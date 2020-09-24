9baba73e48f17bab1afbef246b09ef62779ee152
Headpiker <50620285+Headpiker@users.noreply.github.com>
2020-09-24 15:53:53 +0200

    Slideshow [FUNKAR]
    Reminder to self: Skriptreferenser ska läggas LÄNGST JÄVLA NER I DOKUMENTET för annars funkar inte skriptskiten. Älskar JS.

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,33 @@ function myFunction() {
       x.className = "navbar"; //Om responsive är aktivt så körs den här istället och resettar menyn till original
     }
   }
+
+  var slides = document.querySelectorAll('#slides .slide');
+  var currentSlide = 0;
+  var slideInterval = setInterval(nextSlide,2000);
+  
+  function nextSlide(){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+  }
+  
+  var playing = true;
+  var pauseButton = document.getElementById('pause');
+  
+  function pauseSlideshow(){
+    pauseButton.innerHTML = 'Play';
+    playing = false;
+    clearInterval(slideInterval);
+  }
+  
+  function playSlideshow(){
+    pauseButton.innerHTML = 'Pause';
+    playing = true;
+    slideInterval = setInterval(nextSlide,2000);
+  }
+  
+  pauseButton.onclick = function(){
+    if(playing){ pauseSlideshow(); }
+    else{ playSlideshow(); }
+  }
