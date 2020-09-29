8fc82a69e193ea8f268a7d3e145a45c14ab19282
Headpiker <50620285+Headpiker@users.noreply.github.com>
2020-09-29 12:16:17 +0200

    Regex och animering
    Regex klar, även slängt in en väldigt simpel animering på startsidan

--- JS/regex.js
+++ JS/regex.js
@@ -1,36 +1,63 @@
 function checkName(){
     var namn = document.getElementById('namn').value;    
+    var submit = document.getElementById("submit").disabled = true;
     
-    //Kollar så namnet är minst 3 och max 15
-    if(!/^[a-z0-9_\-]{3,15}$/i.test(namn)){
-        alert("Username should be between 3-15 characters!");
+    //Kollar så namnet är minst 3
+    if(!/[a-z_\-]{3,}$/i.test(namn)){
+        alert("Name should be atleast 3 characters!");
+        isValid = false;
         
     }
     //Kollar för whitespace
     if(/^\s*$/i.test(namn)){
         alert("You need to enter a name!");
+        isValid = false;
         
     }
     
 }
 
-function checkMail(){
-    var mail = document.getElementById("email").value;
-    var regCheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+function checkMail(){ //Funktion för att kolla så att mailen är korrekt angiven och i rätt format
+    var mail = document.getElementById("email");
+    var mailCheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
    
-    if(!regCheck.test(mail.value))
+    if(!mailCheck.test(mail.value)) //Om fältet inte har rätt värde exekveras denna
     {
         alert("Email isn't correctly formatted!");
     }
 }
 
-function checkNumber(){
+function checkNumber(){ //Kollar så att numret är korrekt angivet
     var number = document.getElementById("number");    
+    var numberCheck = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+    
+
+    if(!numberCheck.test(number.value)){
+        alert("Phonenumber isn't entered correctly!")
+    }
 }
 
-function toUpper(){
-    var up = document.getElementById("namn").value;
+function checkText(){ //Kollar om det finns whitespace, finns det whitespace så varnar den
+    var text = document.getElementById("meddelande");
+    var textCheck = /^\s*$/;
+    
 
-    up.value = up.value.toUpperCase();
+    if(textCheck.test(text.value)){
+        alert("You need to enter some text!")
+    }
+}
+
+function manageSubmit(){ //Stänger av submit om fälten är tomma.
+    var submitCheck = document.getElementById("submit");
+    var namn = document.getElementById('namn'); 
+    var mail = document.getElementById("email");
+    var number = document.getElementById("number"); 
+    var text = document.getElementById("meddelande");
+
+    if(namn.value != "" && mail.value !="" && number.value !="" && text.value !=""){
+        submitCheck.disabled = false;
+    }
+
+    else { submitCheck.disabled = true; }
 }
